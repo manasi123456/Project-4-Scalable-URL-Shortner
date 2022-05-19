67eeb9950008aece38939d0936a94ad7fb910514
Manasi Joshi <manasi04joshi@gmail.com>
2022-05-19 19:59:22 +0530

    project04 final solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,19 @@
+// const express = require('express');
+// const router = express.Router();
+// const urlController = require("../Controllers/urlController")
+
+
+// router.post("/url/shorten", urlController.shortUrl)
+// router.get("/:urlCode", urlController.getShortUrl)
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const urlController = require("../Controllers/urlController")
 
 
-router.post("/url/shorten", urlController.shortUrl)
-router.get("/:urlCode", urlController.getShortUrl)
+router.post("/url/shorten", urlController.shortenUrl)
+router.get("/:urlCode", urlController.redirectUrl)
 
 module.exports = router;
